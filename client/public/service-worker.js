d6cde86b22ad10c5bedd2a5cacb8438aab3900d3
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-12-03 09:39:19 +0100

    Aufgabe 7

--- client/public/service-worker.js
+++ client/public/service-worker.js
@@ -1,14 +1,14 @@
 /* global workbox */
 if (workbox) {
-    console.log(`Workbox is loaded`);
-    workbox.setConfig({ debug: true });
-    workbox.precaching.precacheAndRoute(self.__precacheManifest);
-    workbox.routing.registerRoute(
-    '/employees',
+  console.log(`Workbox is loaded`);
+  workbox.setConfig({ debug: true });
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  workbox.routing.registerRoute(
+    "/employees",
     new workbox.strategies.NetworkFirst({
-    cacheName: 'roberts-cache',
-    }),
-    );
-    } else {
-    console.log(`Workbox didn't load`);
-    }
+      cacheName: "roberts-cache",
+    })
+  );
+} else {
+  console.log(`Workbox didn't load`);
+}
