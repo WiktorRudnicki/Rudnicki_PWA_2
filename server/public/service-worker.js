e641d980ba3f0ac5226b069ea57eebac0e20a97a
WiktorRudnicki <rudnicki.w01@htlwienwest.at>
2021-12-03 14:16:57 +0100

    Aufgabe 6

--- server/public/service-worker.js
+++ server/public/service-worker.js
@@ -11,19 +11,26 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
-
-importScripts(
-  "/precache-manifest.b86e99b098e8c5e248b43875d60c9b5e.js"
+ importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js"
 );
 
-workbox.core.setCacheNameDetails({prefix: "employees-simple-pwa"});
+importScripts("/precache-manifest.b86e99b098e8c5e248b43875d60c9b5e.js");
 
-self.addEventListener('message', (event) => {
-  if (event.data && event.data.type === 'SKIP_WAITING') {
+workbox.core.setCacheNameDetails({ prefix: "employees-simple-pwa" });
+
+self.addEventListener("message", (event) => {
+  if (event.data && event.data.type === "SKIP_WAITING") {
     self.skipWaiting();
   }
 });
+
+import {registerRoute} from 'workbox-routing';
+
+registerRoute(
+  'http://localhost:3000/employees',
+  handler
+);
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -32,3 +39,15 @@ self.addEventListener('message', (event) => {
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+if (workbox) {
+  console.log(`Workbox is loaded`);
+  workbox.precaching.precacheAndRoute(self.__precacheManifest);
+  workbox.routing.registerRoute(
+    "/employees",
+    new workbox.strategies.NetworkFirst({
+      cacheName: "roberts-cache",
+    })
+  );
+} else {
+  console.log(`Workbox didn't load`);
+}
